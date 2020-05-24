f30d0800d4652eed8883b92220658b563f200893
Leonardo Augusto <leo.augustobh13@gmail.com>
2020-05-24 11:13:22 -0300

    Padronização da classe Parser

--- src/com/leonardo/logparser/quake/Parser.java
+++ src/com/leonardo/logparser/quake/Parser.java
@@ -9,23 +9,31 @@ import br.com.leonardo.logparser.exception.ReadLogException;
 import br.com.leonardo.logparser.file.Reader;
 import br.com.leonardos.logparser.model.Game;
 
+
 public class Parser {
 
     public static void main(String[] args) {
-        System.out.println("Escolha o diretorio onde está localizado o arquivo: ");
 
+        System.out.println("Escolha o diretorio do arquivo: ");
+        @SuppressWarnings("resource")
         final String path = new Scanner(System.in).next();
 
-        List<String> linhas;
+        // Read all lines from Quake Log
+        List<String> lines;
         try {
-            linhas = Reader.leLines(path);
+            lines = Reader.readLines(path);
         } catch (ReadLogException | NotFoundLogException e) {
             System.out.println(e.getMessage());
             return;
         }
 
+
         final List<Game> games = GameParser.execute(lines);
+
 
         Display.printGames(games);
     }
+
+}
+
 }
