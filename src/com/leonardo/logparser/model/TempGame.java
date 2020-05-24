bde360631c3bc38bbd3f4f012e117ff2e762c9ad
Leonardo Augusto <leo.augustobh13@gmail.com>
2020-05-24 11:07:35 -0300

    Criação da Classe TempGame

---
+++ src/com/leonardo/logparser/model/TempGame.java
@@ -0,0 +1,41 @@
+package com.leonardo.logparser.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TempGame {
+
+    private String nome;
+
+    private List<String> linhas;
+
+    public TempGame() {
+        this.linhas = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setName(String nome) {
+        this.nome = nome;
+    }
+
+    public List<String> getLinhas() {
+        return linhas;
+    }
+
+
+    public void setLinhas(List<String> linhas) {
+        this.linhas = linhas;
+    }
+
+
+    public void addLinha(String linhas) {
+        this.linhas.add(linhas);
+    }
+
+
+}
+
